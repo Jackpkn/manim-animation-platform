7f4f74b8a9bd5627b4e2e81defdcbd7d11155a9b
jackpk <pawankumarbairwa879@gmail.com>
2025-05-05 17:11:40 +0530

    Clean up partial movie file lists and delete unnecessary cache files

--- tmp/3106915f-f611-4485-bc14-642e3e75e80a.py
+++
@@ -1,8 +0,0 @@
-from manim import *
-
-class MyAnimation(Scene):
-    def construct(self):
-        # Your animation code here
-        circle = Circle()
-        self.play(Create(circle))
-        self.wait(1)
